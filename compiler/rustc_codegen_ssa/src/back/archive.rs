755e97573d983449e54dace586c87c65c9f16aaf
Adam Szkoda <adaszko@gmail.com>
2022-05-20 13:54:28 +0200

    Fix issue #96943

--- compiler/rustc_codegen_ssa/src/back/archive.rs
+++ compiler/rustc_codegen_ssa/src/back/archive.rs
@@ -4,6 +4,7 @@ use rustc_session::Session;
 use rustc_span::symbol::Symbol;
 
 use std::io;
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 pub(super) fn find_library(
@@ -31,6 +32,17 @@ pub(super) fn find_library(
             let test = path.join(&unixlibname);
             if test.exists() {
                 return test;
+            }
+            if cfg!(target_os = "macos") {
+                // This is a bit unintuitive: On macOS, a shared library may not be present on the file
+                // system yet still be loadable.  This is due to the fact that there's a global cache of shared libraries being
+                // maintained by the system on BigSur and newer [1].  Therefore, as a last resort, try loading the library
+                // instead of just checking for its existence as a file.
+                // [1]: https://web.archive.org/web/20220317152936/https://developer.apple.com/documentation/macos-release-notes/macos-big-sur-11-beta-release-notes
+                let path: &OsStr = test.as_ref();
+                if let Ok(_) = unsafe { libloading::Library::new(path) } {
+                    return test;
+                }
             }
         }
     }
